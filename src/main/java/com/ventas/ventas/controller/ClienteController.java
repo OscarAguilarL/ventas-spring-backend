f45be989e868c982430204c9c804412ffbc25ed4
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-08-10 17:31:37 -0500

    Exponer un recurso particular con GET y crear recurso con POST

--- src/main/java/com/ventas/ventas/controller/ClienteController.java
+++ src/main/java/com/ventas/ventas/controller/ClienteController.java
@@ -3,10 +3,9 @@ package com.ventas.ventas.controller;
 import com.ventas.ventas.model.Cliente;
 import com.ventas.ventas.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,17 @@ public class ClienteController {
     @GetMapping
     public ResponseEntity<List<Cliente>> findAll() {
         return ResponseEntity.ok(clienteService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> findById(@PathVariable("id") Integer id) {
+        return clienteService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
+        return new ResponseEntity<>(clienteService.create(cliente), HttpStatus.CREATED);
     }
 }
