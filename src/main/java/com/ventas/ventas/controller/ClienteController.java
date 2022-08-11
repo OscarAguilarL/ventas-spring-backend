272d14493633f691cf6757b2a41587ed6c0efa88
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-08-10 21:57:42 -0500

    Modificar datos a través de PUT

--- src/main/java/com/ventas/ventas/controller/ClienteController.java
+++ src/main/java/com/ventas/ventas/controller/ClienteController.java
@@ -31,4 +31,11 @@ public class ClienteController {
     public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
         return new ResponseEntity<>(clienteService.create(cliente), HttpStatus.CREATED);
     }
+
+    @PutMapping
+    public ResponseEntity<Cliente> update(@RequestBody Cliente cliente) {
+        return clienteService.findById(cliente.getId())
+                .map(c -> ResponseEntity.ok(clienteService.update(cliente)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
